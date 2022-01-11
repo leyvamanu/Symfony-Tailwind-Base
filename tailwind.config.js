daeca2be992a5fae10e63194eaca387d178cbca2
leyvamanu <leyvamanu@gmail.com>
2022-01-11 22:21:45 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './templates/**/*.html.twig',
+    './assets/**/*.css',
+    './assets/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
